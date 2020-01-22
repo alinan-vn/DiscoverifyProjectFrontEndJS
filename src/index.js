de25022524542137166dc0ed9781e67637c64a0e
github username <github email address>
2020-01-22 14:49:12 -0600

    cleaned up code and took out forms and replaced with a simpler style. Post needs to be linked

--- src/index.js
+++ src/index.js
@@ -1,110 +1,26 @@
 const BASE_URL = "http://localhost:3000"
-const ARTISTS_URL = `${BASE_URL}/artists`
+const ARTISTS_URL = 'http://localhost:3000/artists'
 const GENRES_URL = `${BASE_URL}/genres`
 const findBody = document.querySelector('body')
 
 document.addEventListener("DOMContentLoaded", function() {
-    clickyBoy();
+    clickListener();
     fetchArtists();
     fetchGenres();
 });
 
 // click events for create artist and create genre buttons
-function clickyBoy() {
+function clickListener() {
     document.addEventListener('click', function(e){
         console.log(e.target)
-        const createArtistBtn = document.getElementById('create-artist-btn');
-        const genreNumBtn = document.getElementById('genre-num-btn');
-        const artistFormBtn = document.getElementById('artist-btn');
-        if (e.target === createArtistBtn){
-            askGenreForm();
-        } else if (e.target === genreNumBtn){
-            createArtistForm();
-        } else if (e.target === artistFormBtn) {
-            postNewArtist();
-        }
-
     })
 };
 
-function removeFirstButtons(){
-    const mainfirst = document.getElementById('buttons');
-    mainfirst.innerHTML = '';
-}
-
-function askGenreForm(){
-    const mainButtons = document.getElementById('buttons');
-    const howManyGenresInput = document.createElement('input');
-    const p = document.createElement('p');
-    const genreNumBtn = document.createElement('button');
-
-
-    p.innerText = 'How many genres will this artist have?';
-    howManyGenresInput.placeholder = 'Number'
-    howManyGenresInput.id = 'genre-num-input';
-    genreNumBtn.id = 'genre-num-btn'
-    genreNumBtn.innerText = 'Create Form';
-    removeFirstButtons();
-
-    mainButtons.append(p, howManyGenresInput, genreNumBtn);
-};
-
-function createArtistForm(){
-    const artistNameInput = document.createElement('input');
-    artistNameInput.id = 'artist-input';
-    artistNameInput.placeholder = 'Artist Name';
-    const mainButtons = document.getElementById('buttons');
-    const artistCreateBtn = document.createElement('button')
-    artistCreateBtn.innerText = 'Create Artist!';
-    artistCreateBtn.id = 'artist-btn';
-
-    const numGenreStr = document.getElementById('genre-num-input');
-    let genreInt = parseInt(numGenreStr.value);
-    console.log(genreInt);
-    removeFirstButtons();
-    mainButtons.append(artistNameInput);
-    for (let i = 0; i < genreInt; i++){
-        const genreInput = document.createElement('input');
-        genreInput.id = `genre-input`
-        genreInput.placeholder = `Genre ${i +1}`;
-        mainButtons.append(genreInput);
-    };
-    mainButtons.append(artistCreateBtn);
-};
-
-// creates fetch request for new artist from form field
-function postNewArtist(){
-    const artistName = document.getElementById('artist-input');
-    const genreInputList = document.querySelectorAll('input#genre-input');
-    const artistInfo = {'name': '', 'genre': []};
-
-    artistInfo['name'] = artistName.value;
-    genreInputList.forEach(function(elem){
-        let genre = elem.value;
-        artistInfo['genre'].push(genre)
-    })
-    console.log(artistInfo)
-    const reqObj = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        },
-        body: JSON.stringify(artistInfo)
-    }
-    
-    fetch(ARTISTS_URL, reqObj)
-        .then(resp => resp.json())
-        .then(artObj => renderArtist(artObj))
-}
-    //fetch existing artists from db
-    
+//fetch existing artists from db    
 function fetchArtists(){
     return fetch(ARTISTS_URL)
         .then(resp => resp.json())
-        .then(artists => 
-        // console.log(artists)
-        iterateArtists(artists))
+        .then(artists => iterateArtists(artists))
 }
         
 //takes array of artists and passes each artist element into renderArtist
@@ -114,7 +30,7 @@ function iterateArtists(artists){
         
 //render individual artist obj on page
 function renderArtist(artObj){
-    const mainSec = document.querySelector('main')
+    const mainSec = document.getElementById('artists-list')
 
     const divWithCard = document.createElement('div')
     divWithCard.classList.add('card')
@@ -135,8 +51,7 @@ function renderArtist(artObj){
         ul.append(li)
     }),
     mainSec.append(divWithCard)
-}
-        
+}       
         
 // creates fetch request for new genre from form field
 function postNewGenre(genreField){
@@ -153,49 +68,47 @@ function postNewGenre(genreField){
         //   "image": artistInfo,
         //   "likes": 0
         })
-    }
+    };
     
     fetch(GENRES_URL, reqObj)
         .then(resp => resp.json())
         .then(genreObj => renderGenre(genreObj))
+};
+
+//fetch existing genres from db
+function fetchGenres(){
+    return fetch(GENRES_URL)
+    .then(resp => resp.json())
+    .then(genres => iterateGenres(genres))
+};
+        
+//takes array of genres and passes each genre element into renderGenre
+function iterateGenres(genres){
+    genres.forEach(renderGenre);
+};
+        
+//render individual genre obj on page
+function renderGenre(genreObj){
+    const mainSec = document.getElementById('genres-list');
+
+    const divWithCard = document.createElement('div');
+    divWithCard.classList.add('card');
+    divWithCard.id = genreObj.id;
+    
+    const h5 = document.createElement('h5');
+    h5.innerText = genreObj.name;
+
+    const p = document.createElement('p');
+    p.innerText = 'Artists';
+
+    const ul = document.createElement('ul');
+    divWithCard.append(h5, p, ul);
+
+    genreObj.artists.forEach(function(artLi){
+        const li = document.createElement('li');
+        li.innerText = artLi.name;
+        ul.append(li);
+    });
+    
+    mainSec.append(divWithCard);
 }
-
-    //fetch existing genres from db
-    function fetchGenres(){
-        return fetch(GENRES_URL)
-        .then(resp => resp.json())
-        .then(genres => 
-            // console.log(genres)
-            iterateGenres(genres))
-        }
-        
-        //takes array of genres and passes each genre element into renderGenre
-        function iterateGenres(genres){
-            genres.forEach(renderGenre)
-        }
-        
-        //render individual genre obj on page
-        function renderGenre(genreObj){
-            const mainSec = document.querySelector('main')
-
-            const divWithCard = document.createElement('div')
-            divWithCard.classList.add('card')
-            divWithCard.id = genreObj.id
-            
-            const h5 = document.createElement('h5')
-            h5.innerText = genreObj.name
-
-            const p = document.createElement('p')
-            p.innerText = 'Artists'
-
-            const ul = document.createElement('ul')
-            divWithCard.append(h5, p, ul)
-
-            genreObj.artists.forEach(function(artLi){
-                const li = document.createElement('li')
-                li.innerText = artLi.name
-                ul.append(li)
-            }),
-
-            mainSec.append(divWithCard)
-        }
--- src/sample.js
+++ src/sample.js
@@ -1,7 +1,19 @@
-const BASE_URL = "http://localhost:3000"
-const ARTISTS_URL = `${BASE_URL}/artists`
-const GENRES_URL = `${BASE_URL}/genres`
-const findBody = document.querySelector('body')
+function clickyBoy() {
+    document.addEventListener('click', function(e){
+        console.log(e.target)
+        const createArtistBtn = document.getElementById('create-artist-btn');
+        const genreNumBtn = document.getElementById('genre-num-btn');
+        const artistFormBtn = document.getElementById('artist-btn');
+        if (e.target === createArtistBtn){
+            askGenreForm();
+        } else if (e.target === genreNumBtn){
+            createArtistForm();
+        } else if (e.target === artistFormBtn) {
+            postNewArtist();
+        }
+
+    })
+};
 
 function removeFirstButtons(){
     const mainfirst = document.getElementById('buttons');
@@ -9,7 +21,6 @@ function removeFirstButtons(){
 }
 
 function askGenreForm(){
-    console.log('in artist creation')
     const mainButtons = document.getElementById('buttons');
     const howManyGenresInput = document.createElement('input');
     const p = document.createElement('p');
@@ -27,8 +38,6 @@ function askGenreForm(){
 };
 
 function createArtistForm(){
-    // removeFirstButtons();
-    console.log('remove first buttons')
     const artistNameInput = document.createElement('input');
     artistNameInput.id = 'artist-input';
     artistNameInput.placeholder = 'Artist Name';
@@ -44,32 +53,35 @@ function createArtistForm(){
     mainButtons.append(artistNameInput);
     for (let i = 0; i < genreInt; i++){
         const genreInput = document.createElement('input');
-        genreInput.id = `genre-input-${i}`
+        genreInput.id = `genre-input`
         genreInput.placeholder = `Genre ${i +1}`;
         mainButtons.append(genreInput);
     };
     mainButtons.append(artistCreateBtn);
 };
 
-document.addEventListener("DOMContentLoaded", function() {
-    clickyBoy();
-    
-});
+// creates fetch request for new artist from form field
+function postNewArtist(){
+    const artistName = document.getElementById('artist-input');
+    const genreInputList = document.querySelectorAll('input#genre-input');
+    const artistInfo = {'name': '', 'genres': []};
 
-// just looks at click events anywhere on page
-function clickyBoy() {
-    document.addEventListener('click', function(e){
-        console.log(e.target)
-        const createArtistBtn = document.getElementById('create-artist-btn');
-        const genreNumBtn = document.getElementById('genre-num-btn');
-        const artistFormBtn = document.getElementById('artist-btn');
-        if (e.target === createArtistBtn){
-            askGenreForm();
-        } else if (e.target === genreNumBtn){
-            createArtistForm();
-        } else if (e.target === artistFormBtn) {
-            postNewArtist();
-        }
-
+    artistInfo['name'] = artistName.value;
+    genreInputList.forEach(function(elem){
+        let genre = elem.value;
+        artistInfo['genres'].push(genre)
     })
-};
+    console.log(artistInfo)
+    const reqObj = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(artistInfo)
+    }
+
+    return fetch(ARTISTS_URL, reqObj)
+        .then(resp => resp.json())
+        .then(json => console.log(json))
+}
